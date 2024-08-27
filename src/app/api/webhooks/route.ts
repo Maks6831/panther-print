54de3f0481f4319c779821607dd4149b4a7eca69
Muhammad <ma.khan6971@gmail.com>
2024-08-27 01:55:39 +0100

    fixed webhook email route import

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -4,7 +4,7 @@ import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 import { Resend } from 'resend'
-import OrderReceivedEmail from '@/components/emails/OrderReceivedEmail'
+import OrderReceivedEmail from '@/components/emails/OrderRecievedEmail'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
